c421e104fff28831d8f5d2223dd8de589037759b
JP Richardson <jprichardson@gmail.com>
2013-05-02 16:11:58 -0500

    tests pass for new open/close templates

--- test/rock.test.js
+++ test/rock.test.js
@@ -8,11 +8,12 @@ var assert = require('assert')
   , testutil = require('testutil')
 
 var TEST_PATH = ''
-  , ROCK_CONF = P('test/resources/rockconf.json')
-  , rockRepo = P('test/resources/rocks/node-lib');
+
+var rockRepo1 = P('test/resources/rocks/node-lib')
+  , rockRepo2 = P('test/resources/rocks/node-lib-tmpl');
 
 function AFE(file1, file2) {
-  T (fs.readFileSync(file1).toString() === fs.readFileSync(file2).toString());
+  EQ (fs.readFileSync(file1).toString(), fs.readFileSync(file2).toString());
 }
 
 describe('rock', function(){
@@ -22,57 +23,68 @@ describe('rock', function(){
   })
 
   describe('+ fetchRepo()', function(){
-    it('should generate a basic project', function(done){
-      var testPath = path.join(TEST_PATH, 'create')
-        , debugFile = path.join(testPath, 'debug.txt')
-        , appName = 'myapp'
-        , projectName = 'cool_module';
-
-      next({
-        ERROR: function(err) {
-          done(err); //FAIL
-        },
-        makeTestDir: function() {
-          fs.mkdir(testPath, this.next);
-        },
-        rockCreate: function(){
-          process.chdir(testPath);
-            
-          var templateValues = {
-            'author': 'JP Richardson',
-            'email': 'jprichardson@gmail.com',
-            'project-description': 'A cool test for a sweet library.',
-            'project-name': 'cool_module'
-          };
-
-          rock.fetchRepo(appName, rockRepo, {templateValues: templateValues}, this.next);
-        },
-        verifyResults: function() {
-          var outDir = path.join(path.join(testPath, appName));
-          var expectDir = P('test/resources/expect/' + appName);
-
-          function AF(file) {
-            var file1 = path.join(outDir, file);
-            var file2 = path.join(expectDir, file);
-            AFE(file1, file2);
-          }
-
-          assert(fs.existsSync(outDir));
-
-          AF('LICENSE');
-          AF('README.md');
-          AF('lib/' + projectName + '.js');
-          AF('test/' + projectName + '.test.js');
-          AF('ignore_this/READTHIS.md');
-
-          assert(!fs.existsSync(path.join(outDir, '.git')));
-          assert(!fs.existsSync(path.join(outDir, '.rock')));
-
-          done()
-        }
+    describe('> when default settings', function() {
+      it('should generate a basic project', function(done){
+        TEST(rockRepo1, done)
       });
-    });
+    })
 
+    describe('> when change open and closing templates', function() {
+      it('should generate a basic project', function(done){
+        TEST(rockRepo2, done)
+      });
+    })
   });
 });
 
+
+function TEST (rockRepo, done) {
+  var testPath = path.join(TEST_PATH, 'create')
+    , debugFile = path.join(testPath, 'debug.txt')
+    , appName = 'myapp'
+    , projectName = 'cool_module';
+
+  next({
+    ERROR: function(err) {
+      done(err); //FAIL
+    },
+    makeTestDir: function() {
+      fs.mkdir(testPath, this.next);
+    },
+    rockCreate: function(){
+      process.chdir(testPath);
+        
+      var templateValues = {
+        'author': 'JP Richardson',
+        'email': 'jprichardson@gmail.com',
+        'project-description': 'A cool test for a sweet library.',
+        'project-name': 'cool_module'
+      };
+
+      rock.fetchRepo(appName, rockRepo, {templateValues: templateValues}, this.next);
+    },
+    verifyResults: function() {
+      var outDir = path.join(path.join(testPath, appName));
+      var expectDir = P('test/resources/expect/' + appName);
+
+      function AF(file) {
+        var file1 = path.join(outDir, file);
+        var file2 = path.join(expectDir, file);
+        AFE(file1, file2);
+      }
+
+      assert(fs.existsSync(outDir));
+
+      AF('LICENSE');
+      AF('README.md');
+      AF('lib/' + projectName + '.js');
+      AF('test/' + projectName + '.test.js');
+      AF('ignore_this/READTHIS.md');
+
+      assert(!fs.existsSync(path.join(outDir, '.git')));
+      assert(!fs.existsSync(path.join(outDir, '.rock')));
+
+      done()
+    }
+  });
+}
