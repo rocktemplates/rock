87a7cacd27be45f1cdb95be80166a4a10137a1e9
RyanZim <opensrc@ryanzim.com>
2016-09-06 14:03:33 -0400

    Add standard
    Edit code to comply

--- test/rock-bin.test.js
+++ test/rock-bin.test.js
@@ -1,73 +1,72 @@
+/* eslint-env mocha */
 var P = require('autoresolve')
-  , suppose = require('suppose')
-  , path = require('path-extra')
-  , fs = require('fs-extra')
-  , testutil = require('testutil')
+var suppose = require('suppose')
+var path = require('path-extra')
+var fs = require('fs-extra')
+var testutil = require('testutil')
 
 var ROCK_CMD = P('bin/rock')
-  , TEST_PATH = null
-  , ROCK_CONF = P('test/resources/rock.conf.json')
-  , testRockPath = P('test/resources/rocks/node-lib')
+var TEST_PATH = null
+var ROCK_CONF = P('test/resources/rock.conf.json')
+var testRockPath = P('test/resources/rocks/node-lib')
 
-function AFE(file1, file2) {
-  T (fs.readFileSync(file1).toString() === fs.readFileSync(file2).toString())
+function AFE (file1, file2) {
+  T(fs.readFileSync(file1).toString() === fs.readFileSync(file2).toString())
 }
 
-describe('rock-bin', function(){
-  beforeEach(function(){
+describe('rock-bin', function () {
+  beforeEach(function () {
     TEST_PATH = testutil.createTestDir('rock')
   })
 
-  it('should generate a basic project', function(done){
+  it('should generate a basic project', function (done) {
     var testPath = path.join(TEST_PATH, 'exec_from_cmd')
-      , debugFile = path.join(testPath, 'debug.txt')
-      , appName = 'myapp'
-      , projectName = 'cool_module'
-      , rockGitDir = path.join(testRockPath, '.git')
-      , cwd = process.cwd()
+    var appName = 'myapp'
+    var projectName = 'cool_module'
+    var rockGitDir = path.join(testRockPath, '.git')
+    var cwd = process.cwd()
 
       // Make test directory:
-      fs.mkdir(testPath, executeRock)
-      function executeRock(){
-        process.chdir(cwd)
+    fs.mkdir(testPath, executeRock)
+    function executeRock () {
+      process.chdir(cwd)
 
-        process.chdir(testPath)
-        suppose(ROCK_CMD, [appName, '-c', ROCK_CONF, '-r', P('test/resources/rocks/node-lib')])
-          //.debug(process.stdout)
+      process.chdir(testPath)
+      suppose(ROCK_CMD, [appName, '-c', ROCK_CONF, '-r', P('test/resources/rocks/node-lib')])
+          // .debug(process.stdout)
           .on('author: ').respond('JP Richardson\n')
           .on('email: ').respond('jprichardson@gmail.com\n')
           .on('project-description: ').respond('A cool test for a sweet library.\n')
           .on('project-name: ').respond(projectName + '\n')
           .error(done)
           .end(verifyResults)
+    }
+    function verifyResults (code) {
+      T(code === 0)
+
+      var outDir = path.join(path.join(testPath, appName))
+      var expectDir = P('test/resources/expect/' + appName)
+
+      function AF (file) {
+        var file1 = path.join(outDir, file)
+        var file2 = path.join(expectDir, file)
+        AFE(file1, file2)
       }
-      function verifyResults(code) {
-        T (code === 0)
 
-        var outDir = path.join(path.join(testPath, appName))
-        var expectDir = P('test/resources/expect/' + appName)
+      if (fs.existsSync(rockGitDir)) fs.removeSync(rockGitDir)
 
-        function AF(file) {
-          var file1 = path.join(outDir, file)
-          var file2 = path.join(expectDir, file)
-          AFE(file1, file2)
-        }
+      T(fs.existsSync(outDir))
 
-        if (fs.existsSync(rockGitDir))
-          fs.removeSync(rockGitDir)
+      AF('LICENSE')
+      AF('README.md')
+      AF('lib/' + projectName + '.js')
+      AF('test/' + projectName + '.test.js')
+      AF('ignore_this/READTHIS.md')
 
-        T (fs.existsSync(outDir))
+      T(!fs.existsSync(path.join(outDir, '.git')))
+      T(!fs.existsSync(path.join(outDir, '.rock')))
 
-        AF('LICENSE')
-        AF('README.md')
-        AF('lib/' + projectName + '.js')
-        AF('test/' + projectName + '.test.js')
-        AF('ignore_this/READTHIS.md')
-
-        T (!fs.existsSync(path.join(outDir, '.git')))
-        T (!fs.existsSync(path.join(outDir, '.rock')))
-
-        done()
-      }
+      done()
+    }
   })
 })
--- test/rock-file.test.js
+++ test/rock-file.test.js
@@ -1,34 +1,34 @@
+/* eslint-env mocha */
 var P = require('autoresolve')
-  , path = require('path-extra')
-  , fs = require('fs-extra')
-  , rock = require(P('lib/rock'))
-  , testutil = require('testutil')
-  , nock = require('nock')
+var path = require('path-extra')
+var fs = require('fs-extra')
+var rock = require(P('lib/rock'))
+var testutil = require('testutil')
+var nock = require('nock')
 
 var TEST_DIR = null
 
-var TMPL = "Hi, @@author@@ is going to build:\n@@project-name@@."
-  , TMPL_E = "Hi, JP is going to build:\nRock."
+var TMPL = 'Hi, @@author@@ is going to build:\n@@project-name@@.'
+var TMPL_E = 'Hi, JP is going to build:\nRock.'
 
-describe('rock', function(){
-  beforeEach(function(){
-    TEST_DIR = testutil.createTestDir('rock');
+describe('rock', function () {
+  beforeEach(function () {
+    TEST_DIR = testutil.createTestDir('rock')
     TEST_DIR = path.join(TEST_DIR, 'fetch-file')
   })
 
-  describe('+ fetchFile()', function(){
-    describe('> when change open and closing templates', function() {
-      it('should generate a basic project', function(done){
+  describe('+ fetchFile()', function () {
+    describe('> when change open and closing templates', function () {
+      it('should generate a basic project', function (done) {
         TEST(done)
-      });
+      })
     })
-  });
-});
-
+  })
+})
 
 function TEST (done) {
   var file = path.join(TEST_DIR, 'info.txt')
-    , remote = 'http://localhost/data.txt'
+  var remote = 'http://localhost/data.txt'
 
   nock('http://localhost')
   .get('/data.txt')
@@ -39,9 +39,9 @@ function TEST (done) {
     'project-name': 'Rock'
   }
 
-  rock.fetchFile(file, remote, {templateValues: templateValues, tokens: {open: '@@', close: '@@'}}, function(err) {
-    F (err)
-    EQ (TMPL_E, fs.readFileSync(file, 'utf8'))
+  rock.fetchFile(file, remote, {templateValues: templateValues, tokens: {open: '@@', close: '@@'}}, function (err) {
+    F(err)
+    EQ(TMPL_E, fs.readFileSync(file, 'utf8'))
 
     done()
   })
